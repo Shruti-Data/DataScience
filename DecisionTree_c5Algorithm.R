2e55abe347e9868f04527805bfdfaed1acedb56f
Shruti-Data <54521288+Shruti-Data@users.noreply.github.com>
2019-09-08 15:46:24 +0530

    Add files via upload

---
+++ DecisionTree_c5Algorithm.R
@@ -0,0 +1,13 @@
+install.packages("caret")
+install.packages("C50")
+data("iris")
+library(caret)
+library(C50)
+inTraininglocal<-createDataPartition(iris$Species,p=0.75,list=F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+model<-C5.0(training$Species~.,data=training)
+summary(model)
+pred<-predict.C5.0(model,testing[,-5])
+a<-table(testing$Species,pred)
+sum(diag(a))/sum(a)
